6428e49f66f471281019414360fe79644cb3560a
Mihail Zhelezovskiy <zhelezovskiy07@gmail.com>
2021-02-20 10:40:54 +0300

    hw 11

---
+++ src/main/java/HomeWork11/Main.java
@@ -0,0 +1,4 @@
+package HomeWork11;
+
+public class Main {
+}
---
+++ src/main/java/HomeWork11/PhoneCataloque.java
@@ -0,0 +1,14 @@
+package HomeWork11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneCatalog {
+
+    Map<Integer, String> cataloque = new HashMap<>();
+
+    public void add(int phoneNum, String surname){
+
+    }
+
+}
