2a0cd0aa0087fc05fd2cb0f32ef6fd17f8af2f49
Mihail Zhelezovskiy <zhelezovskiy07@gmail.com>
2021-02-27 11:23:18 +0300

    hw 13

--- src/main/java/HomeWork13/MyThread.java
+++ src/main/java/HomeWork13/MyThread.java
@@ -1,4 +1,19 @@
 package HomeWork13;
 
-public class MyThread {
+public class MyThread extends Thread {
+
+    private float[] arr;
+
+    public MyThread(float[] arr) {
+        this.arr = arr;
+    }
+
+    @Override
+    public void run() {
+        synchronized (arr){
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        }
+    }
 }
