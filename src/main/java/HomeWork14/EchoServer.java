9026f4f660cfd95d77b02c4a67f8677225f23f55
Mihail Zhelezovskiy <zhelezovskiy07@gmail.com>
2021-03-04 21:06:42 +0300

    hw 14

---
+++ src/main/java/HomeWork14/EchoServer.java
@@ -0,0 +1,46 @@
+package HomeWork14;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+
+    public static void main(String[] args) {
+        Socket socket = null;
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String str = in.readUTF();
+                System.out.println(str);
+                if (str.equals("/end")) {
+                    break;
+                }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while (true) {
+                                String name = reader.readLine();
+                                out.writeUTF("Serv: " + name);
+                                if (name.equalsIgnoreCase("/end")) {
+                                    break;
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
