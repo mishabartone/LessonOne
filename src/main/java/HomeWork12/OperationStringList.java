88c289cfbd742d75210d6a21fc75986c07a7c6cf
Mihail Zhelezovskiy <zhelezovskiy07@gmail.com>
2021-03-23 14:40:58 +0300

    lec

--- src/main/java/HomeWork12/OperationStringList.java
+++ src/main/java/HomeWork12/OperationStringList.java
@@ -3,6 +3,6 @@ package HomeWork12;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface OperationString {
-    List<String> needList();
-}
+//public interface OperationString {
+//    List<String> needList();
+//}
