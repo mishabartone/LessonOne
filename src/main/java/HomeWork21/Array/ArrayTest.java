09dbd8f5d5ce0816b3d4f098b260c3919e454c65
Mihail Zhelezovskiy <zhelezovskiy07@gmail.com>
2021-04-05 08:44:49 +0300

    hw 22

--- src/main/java/HomeWork21/Array/ArrayTest.java
+++ src/main/java/HomeWork21/Array/ArrayTest.java
@@ -8,9 +8,27 @@ public class ArrayTest {
     private ArrayFromFour arrayFromFour = new ArrayFromFour();
 
     @Test
-    public void testArrayFour() {
+    public void testArrayFour1() {
         int [] expected = {1, 7};
         int [] original = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        int [] testArr = arrayFromFour.ArrayFromFour(original);
+
+        Assert.assertArrayEquals(testArr, expected);
+    }
+
+    @Test
+    public void testArrayFour2() {
+        int [] expected = {2, 7};
+        int [] original = {4, 2, 7};
+        int [] testArr = arrayFromFour.ArrayFromFour(original);
+
+        Assert.assertArrayEquals(testArr, expected);
+    }
+
+    @Test
+    public void testArrayFour3() {
+        int [] expected = {0};
+        int [] original = {1, 2, 4, 0};
         int [] testArr = arrayFromFour.ArrayFromFour(original);
 
         Assert.assertArrayEquals(testArr, expected);
@@ -46,5 +64,5 @@ public class ArrayTest {
 
         Assert.assertEquals(itsTrue, false);
     }
-    
+
 }
